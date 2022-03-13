3744ddf430e2e201a7c91414a86f6dff7f055995
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-03-13 11:15:10 +0300

    ft:WHAT WE DO SECTION:added toggle effect

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#but").click(function(){
+        $("p2").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#but2").click(function(){
+        $("p3").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#but3").click(function(){
+        $("p4").toggle();
+    });
+});
+
